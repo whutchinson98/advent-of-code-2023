e5d301bf4f7dac0ecb2537b05f15451895502a2b
William Hutchinson <willhutchinson2.0@gmail.com>
2023-12-03 11:17:53 -0500

    day2_part2

--- day02/src/bin/solve.rs
+++ day02/src/bin/solve.rs
@@ -1,9 +1,10 @@
 use std::fs;
-use day02::part_one;
+use day02::{part_one, part_two};
 
 fn main() {
     let contents =
         fs::read_to_string("./input.txt").expect("Should have been able to read the file");
 
     println!("Part 1: {}", part_one(&contents));
+    println!("Part 2: {}", part_two(&contents));
 }
--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -97,6 +97,21 @@ pub fn part_one(input: &str) -> usize {
         .sum::<usize>()
 }
 
+pub fn part_two(input: &str) -> usize {
+    let mut lines: Vec<&str> = input.split("\n").collect();
+    if lines[lines.len() - 1] == "" {
+        lines.pop();
+    }
+    let processed_games = process_games(lines);
+    processed_games
+        .iter()
+        .map(|g| {
+            let count = count_cubes(&g.rounds);
+            count.0 * count.1 * count.2
+        })
+        .sum::<usize>()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -156,6 +171,19 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
             ),
             8
+        );
+    }
+    #[test]
+    fn part_two_test() {
+        assert_eq!(
+            part_two(
+                "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+            ),
+            2286
         );
     }
 }
